84193e4aadf6327e7abb9de5957df6bd7fad411c
aRailsDemo <arailsdemo@gmail.com>
2011-10-27 12:26:03 -0700

    [jasmine] Generate Rails app

---
+++ todo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '095cee3e2bde6f922bfd5f01cb9d28c742480e96eea16046d80c306a91d4381894ccc1933a431e807908b51f71e159c77bd759f1f837ff0ccb272490f688676d'
