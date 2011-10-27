81a3405264e12b28d4a459dc0f0be530cec5f511
aRailsDemo <arailsdemo@gmail.com>
2011-10-27 12:32:14 -0700

    [jasmine] Setup jasminerice

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,4 +1,9 @@
 Todo::Application.routes.draw do
+
+  if ["development", "test"].include? Rails.env
+      mount Jasminerice::Engine => "/jasmine"
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
